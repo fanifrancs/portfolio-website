db0432ea9d990690d2f8f4b7aaeedbc7783962ca
fanifrancs <fanikufran6@gmail.com>
2024-10-22 14:02:28 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Alert from 'react-bootstrap/Alert';
+import NavigationBar from './components/NavigationBar';
 
 import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 
-function Example() {
-  return (
-    <Alert dismissible variant="danger">
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>
-        Change this and that and try again.
-      </p>
-    </Alert>
-  )
-}
-
 function App() {
   return (
     <Router>
-    <Example />
-    //navbar to be displayed on all pages
-      <Navbar />
+      <NavigationBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
